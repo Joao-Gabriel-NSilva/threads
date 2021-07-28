9514fe3cc7544cfceb97daddce2f5e99eb3a8948
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-07-28 16:30:50 -0300

    aula 1, video 3. Criando a primeira thread

--- src/br/com/alura/threads/Principal.java
+++ src/br/com/alura/threads/Principal.java
@@ -2,9 +2,11 @@ package br.com.alura.threads;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
+		System.out.println("isso é a thread main");
 		
+		Thread.sleep(30000);
 	}
 
 }
