f5e90ebc3698240126f325d067c42b1f5ad00dd4
João Gabriel Nascimento da Silva <jgabriel10x@gmail.com>
2021-08-04 13:26:28 -0300

    aula 6, video 2, wait antes do notify

--- src/br/com/alura/lista/TarefaImprime.java
+++ src/br/com/alura/lista/TarefaImprime.java
@@ -11,13 +11,20 @@ public class TarefaImprime implements Runnable {
 	@Override
 	public void run() {
 		
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e1) {
+//			e1.printStackTrace();
+//		}
+		
 		synchronized (lista) {
-			
-			try {
-				System.out.println("indo dormir, esperando notificacao");
-				lista.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();	
+			if(!lista.estaCheia()) {
+				try {
+					System.out.println("indo dormir, esperando notificacao");
+					lista.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();	
+				}				
 			}
 			
 			for (int i = 0; i < lista.tamanho(); i++) {
